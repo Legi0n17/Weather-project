df9f55f0e6589f0bebbb536010caa72fd72f2679
Siddharth <60702056+Legi0n17@users.noreply.github.com>
2021-06-22 17:58:59 +0530

    main

---
+++ weather.py
@@ -0,0 +1,39 @@
+import requests
+#import os
+from datetime import datetime
+
+api_key = '87d845b0b6cf29baa1a73cc34b067a95'
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json() 
+
+#create variables to store and display data
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+#textfile
+weatherfile = open('weather.txt', 'a')
+heading = ("Weather Stats for -{}  || {}".format(location.upper(), date_time))
+
+
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("-------------------------------------------------------------")
+
+print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("Current weather desc  :",weather_desc)
+print ("Current Humidity      :",hmdt, '%')
+print ("Current wind speed    :",wind_spd ,'kmph')
+
+#writing inside textfile 
+weatherfile.write(heading + "\n")
+weatherfile.write("Current temperature is:" + str("{:.2f} deg C".format(temp_city)) + "\n")
+weatherfile.write("Current weather desc :" + str(weather_desc) + "\n")
+weatherfile.write("Current Humidity      :" + str(hmdt) + "\n")
+weatherfile.write("Current wind speed    :" + str(wind_spd) + "\n")
+
+weatherfile.close()
